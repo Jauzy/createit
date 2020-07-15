7ca3dac163ffcab8947535d73e14c00fb2020096
Jauzy <al.zaujy@gmail.com>
2020-07-16 05:54:48 +0700

    update

--- src/Constants/BaseUrl.js
+++ src/Constants/BaseUrl.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
-export default axios.create({ baseURL: 'https://createit-server.et.r.appspot.com' })
+export default axios.create({ baseURL: 'https://vocal-eon-276909.et.r.appspot.com' })
 // export default axios.create({ baseURL: 'http://localhost:5000' })
