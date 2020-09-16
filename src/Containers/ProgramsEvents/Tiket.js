643854058c07d06bdedcefd10abd7b761dc54574
Affan Ahsanul Habib <affanahsan02@gmail.com>
2020-09-16 18:40:47 +0700

    Update

--- src/Containers/ProgramsEvents/Tiket.js
+++ src/Containers/ProgramsEvents/Tiket.js
@@ -12,11 +12,11 @@ const Tiket = (props) => {
     }
     
     const sections = [
-        { harga: 'Mulai dari Rp25.000', name: 'Paket Tiket 1' },
-        { harga: 'Mulai dari Rp50.000', name: 'Paket Tiket 2' },
-        { harga: 'Mulai dari Rp100.000', name: 'Paket Tiket 3' },
-        { harga: 'Mulai dari Rp150.000', name: 'Paket Tiket 4' },
-        { harga: 'Mulai dari Rp300.000', name: 'Paket Tiket 5' },
+        { harga: 'Mulai dari Rp25.000', name: 'Paket Tiket 1', keterangan: 'Daily Ticket' },
+        { harga: 'Mulai dari Rp50.000', name: 'Paket Tiket 2', keterangan: '3 Days Pass' },
+        { harga: 'Mulai dari Rp100.000', name: 'Paket Tiket 3', keterangan: '1 Week Pass' },
+        { harga: 'Mulai dari Rp150.000', name: 'Paket Tiket 4', keterangan: '2 Weeks Pass' },
+        { harga: 'Mulai dari Rp300.000', name: 'Paket Tiket 5', keterangan: 'A Month Pass' },
     ]
 
     useEffect(() => {
@@ -25,14 +25,17 @@ const Tiket = (props) => {
     return (
         <div className='container'>
             <div className='row justify-content-center'>
-                <div className='col-8'>
+                <div className='col-lg-8'>
                     <h3>Design Essentials for Branding</h3>
                     <h6 className='text-secondary'>Minggu, 23 Agustus 2020, 13.00 - 15.00 WIB</h6>
                     <div className='pt-3'>
                         {sections.map(item => (
-                            <div className={'p-4 d-flex align-items-center rounded-lg profile-btn border justify-content-between ' + (state.activeSection == item.name ? '-active' : '')}
+                            <div className={'p-4 d-flex align-items-center justify-content-between rounded-lg tiket-btn' + (state.activeSection == item.name ? '-active' : '')}
                                 onClick={() => setActiveSection(item.name)}>
-                                <h5 className='my-auto ml-1'>{item.name}</h5>
+                                <div>
+                                    <h5 className='my-auto ml-1'>{item.name}</h5>
+                                    {state.activeSection == `${item.name}` && <p className='my-auto ml-1'>{item.keterangan}</p>}
+                                </div>
                                 <p className='my-auto'>{item.harga}</p>
                             </div>
                         ))}
